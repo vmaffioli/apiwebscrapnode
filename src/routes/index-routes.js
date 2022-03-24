4b40a6e37417756ff197543d4cb96e2d9114f5a2
vmaffioli <vinimaffioli@gmail.com>
2022-03-24 02:35:02 -0300

    initial commit

---
+++ src/routes/index-routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+var ig = require('instagram-scraping');
+
+//get instagram posts by tag and user
+router.get(`/api/igscrap/`, (req, res) => {
+    if (
+        req.query.tag !== undefined ||
+        req.query.user !== undefined
+    ) {
+        ig.scrapeTag(req.query.tag).then((result) => {
+            let medias = result.medias
+            let posts = [];
+
+            medias.forEach(media => {
+                if (media.node.owner.id === req.query.user)
+                    posts.push(media);
+            });
+            res.status(200).send(
+                posts
+            );
+        });
+    } else {
+        res.status(400).send(
+            "bad request syntax"
+        );
+    }
+});
+
+//get instagram posts by tag
+router.get(`/api/igscrap/test/`, (req, res) => {
+    if (req.query.tag !== undefined) {
+        ig.scrapeTag(req.query.tag).then((result) => {
+            let medias = result.medias
+            res.status(200).send(
+                medias
+            );
+        });
+    } else {
+        res.status(400).send(
+            "bad request syntax"
+        );
+    }
+});
+
+module.exports = router;
